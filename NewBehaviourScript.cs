7c741198df6358ba4577d08c97d7cf69201ad5de
TheAkseon <abqwerty390@gmail.com>
2021-08-22 14:05:05 +0300

    init

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string Name;
+
+    private void Start()
+    {
+        Debug.Log("The code works!");
+    }
+}
